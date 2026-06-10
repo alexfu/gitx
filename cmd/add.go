a61eec65e8396b8f0bcbb5a9c6bca8fe8243d02d
Alex Fu <alexfu@fastmail.com>
2026-06-10 08:01:19 -0400

    source extensions from github

--- cmd/add.go
+++ cmd/add.go
@@ -4,41 +4,40 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
-	"path/filepath"
 
-	"gitx/internal/config"
 	"gitx/internal/repo"
 
+	"github.com/go-git/go-git/v6"
 	"github.com/spf13/cobra"
 )
 
 var addCmd = &cobra.Command{
 	Use:          "add",
-	Short:        "A brief description of your command",
+	Short:        "Add extensions",
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		name := args[0]
 
 		// Fetch extension
-		extension, err := repo.DownloadExtention(name)
+		path, err := repo.DownloadExtention(name)
+		if err != nil {
+			if errors.Is(err, git.ErrTargetDirNotEmpty) {
+				fmt.Fprintln(os.Stderr, "Extension already exists!")
+				return nil
+			}
+			return err
+		}
+
+		// Install extension
+		err = repo.InstallExtension(path)
 		if err != nil {
 			return err
 		}
 
-		// Save extension
-		gitxhome, err := config.EnsureGitxHome()
-		if err != nil {
-			return err
-		}
-
-		filename := filepath.Join(gitxhome, fmt.Sprintf("git-%s", name))
-		err = os.WriteFile(filename, extension, 0o644)
-		if err != nil {
-			return err
-		}
-
+		fmt.Println("✅ Extension installed!")
 		return nil
 	},
 }
--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,25 +1,81 @@
 package repo
 
 import (
-	"io"
-	"net/http"
+	"io/fs"
 	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gitx/internal/config"
+
+	"github.com/go-git/go-git/v6"
 )
 
-func DownloadExtention(name string) ([]byte, error) {
+func DownloadExtention(name string) (string, error) {
 	url := url.URL{
-		Scheme: "http",
-		Host:   "localhost:8000",
+		Scheme: "https",
+		Host:   "github.com",
 		Path:   name,
 	}
-	resp, err := http.Get(url.String())
+
+	path, err := extensionPath(name)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	body, err := io.ReadAll(resp.Body)
-	resp.Body.Close()
+
+	_, err = git.PlainClone(path, &git.CloneOptions{
+		URL: url.String(),
+	})
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return body, nil
+
+	return path, nil
+}
+
+func InstallExtension(path string) error {
+	gitxhome, err := config.EnsureGitxHome()
+	if err != nil {
+		return err
+	}
+
+	filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		basename := filepath.Base(path)
+		if strings.HasPrefix(basename, "git-") && isFile(path) {
+			os.Symlink(path, filepath.Join(gitxhome, basename))
+		}
+		return nil
+	})
+	return nil
+}
+
+func extensionPath(name string) (string, error) {
+	extensionDir, err := dataDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(extensionDir, name), nil
+}
+
+func dataDir() (string, error) {
+	if xdgDataHome := os.Getenv("XDG_DATA_HOME"); xdgDataHome != "" {
+		return filepath.Join(xdgDataHome, "gitx"), nil
+	}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(home, ".local", "share", "gitx"), nil
+}
+
+func isFile(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return !info.IsDir()
 }
